cffa8d85ad3f4456ddff799d5de8a6cb123f83db
Tyler Metz <53663318+Tyler-Metz@users.noreply.github.com>
2021-07-06 08:34:05 -0500

    Update RAD Tool (Ship) Checkbox Generator.user.js

--- RAD Tool (Ship) Checkbox Generator.user.js
+++ RAD Tool (Ship) Checkbox Generator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RAD Tool (Ship) Checkbox Generator
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.11
 // @description  Generates checkboxes on RAD Tool Ship
 // @author       Tyler Metz
 // @match        https://rad-operations.supplychain.opstech.a2z.com/ship
