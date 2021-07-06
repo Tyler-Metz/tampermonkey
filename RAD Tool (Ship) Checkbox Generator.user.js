7a95b14576463c0b2df30e2a8100c627583d20d8
Tyler Metz <53663318+Tyler-Metz@users.noreply.github.com>
2021-07-06 08:21:51 -0500

    Update RAD Tool (Ship) Checkbox Generator.user.js

--- RAD Tool (Ship) Checkbox Generator.user.js
+++ RAD Tool (Ship) Checkbox Generator.user.js
@@ -1,40 +1,33 @@
 // ==UserScript==
 // @name         RAD Tool (Ship) Checkbox Generator
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.0
 // @description  Generates checkboxes on RAD Tool Ship
-// @author       You
+// @author       Tyler Metz
 // @match        https://rad-operations.supplychain.opstech.a2z.com/ship
-// @include      https://rad-operations.supplychain.opstech.a2z.com/pick
 // @downloadURL  https://github.com/Tyler-Metz/tampermonkey/blob/d2e08f5c46916040ed774373edd3a07502f17102/RAD%20Tool%20(Ship)%20Checkbox%20Generator.user.js
 // @updateURL    https://github.com/Tyler-Metz/tampermonkey/blob/d2e08f5c46916040ed774373edd3a07502f17102/RAD%20Tool%20(Ship)%20Checkbox%20Generator.user.js
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-console.log("hi");
-
 // Creates button to trigger function
 var wrapButton = document.createElement("div");
+wrapButton.setAttribute("id", "wrapButton");
 var parentWrap = document.querySelectorAll(".container-fluid")[0]
 var wrap = document.getElementById("content");
 wrap.appendChild(wrapButton);
 wrap.insertBefore(wrapButton, parentWrap);
 var inputButton = document.createElement("button");
+inputButton.setAttribute("class", "customButtons");
 inputButton.setAttribute("type", "button");
-inputButton.setAttribute("value", "Generate Checkboxes");
+// inputButton.setAttribute("value", "Generate Checkboxes");
 inputButton.innerHTML = 'Generate Checkboxes';
 wrapButton.appendChild(inputButton);
 
 inputButton.addEventListener("click", function(){
     generateCheckBoxes();
 })
-
-// Inline CSS for Button
-inputButton.style.borderWidth = "2px";
-inputButton.style.borderColor = "orange";
-inputButton.style.marginLeft = "38px";
-inputButton.style.background = "linear-gradient(60deg, orange, white)";
 
 // Toggles Andon Checkbox
 var tickBox = document.querySelectorAll(".ant-checkbox-input");
@@ -59,6 +52,7 @@ function generateCheckBoxes(){
             }
             else if (cells[i].firstChild.children.length == 2){
                 cells[i].firstChild.appendChild(cln)
+                cln.setAttribute("class", "checkbox");
                 cln.style.width = "10px";
                 cln.style.length = "20px";
             }
@@ -66,5 +60,63 @@ function generateCheckBoxes(){
     }
 }
 
+// Makes Save & Load Button
+var saveButton = document.createElement("button");
+var loadButton = document.createElement("button");
+saveButton.setAttribute("class", "customButtons");
+loadButton.setAttribute("class", "customButtons");
+saveButton.textContent = "Save Checkboxes";
+loadButton.textContent = "Load Checkboxes";
+wrapButton.appendChild(saveButton);
+wrapButton.appendChild(loadButton);
+wrapButton.style.display = "flex";
+wrapButton.style.justifyContent = "space-evenly";
 
+// Saves checkbox data to localStorage
+function saveData(){
+    const checkedBoxes = {};
+    const defaultInput = document.querySelectorAll(".ant-input");
+    const defaultInputStr = defaultInput[0].value
+    var allCheckBoxes = document.querySelectorAll(".checkbox");
+    for (var i=0;i < allCheckBoxes.length;i++){
+        switch(allCheckBoxes[i].checked){
+            case true:
+                checkedBoxes[i] = allCheckBoxes[i].checked;
+                continue;
+            case false:
+                continue;
+        }
+    }
+    const checkedBoxesStr = JSON.stringify(checkedBoxes);
+    localStorage.setItem(defaultInputStr, checkedBoxesStr);
+}
 
+// Loads checkbox data from localStorage
+function loadData(){
+    const defaultInput = document.querySelectorAll(".ant-input");
+    const defaultInputStr = defaultInput[0].value
+    var checkBoxData = localStorage.getItem(defaultInputStr);
+    checkBoxData = JSON.parse(checkBoxData);
+    var allCheckBoxes = document.querySelectorAll(".checkbox");
+    console.log(checkBoxData);
+    for (var i in allCheckBoxes){
+        allCheckBoxes[i].checked = checkBoxData[i]
+    }
+
+}
+
+saveButton.addEventListener("click", function(){
+    saveData();
+});
+
+loadButton.addEventListener("click", function(){
+    loadData();
+});
+
+// Inline CSS for all custom buttons
+const allCustomButtons = document.querySelectorAll(".customButtons");
+for (var i = 0;i < allCustomButtons.length;i++){
+    allCustomButtons[i].style.borderWidth = "2px"
+    allCustomButtons[i].style.borderColor = "orange"
+    allCustomButtons[i].style.background = "linear-gradient(60deg, orange, white)";
+}
